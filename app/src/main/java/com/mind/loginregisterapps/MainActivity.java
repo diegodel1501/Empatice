0ad11307a2f66c27840ad504f792cb4c83f3053d
joossesp1 <joossesp@gmail.com>
2019-05-31 13:08:53 -0400

    Initial commit

---
+++ app/src/main/java/com/mind/loginregisterapps/MainActivity.java
@@ -0,0 +1,125 @@
+package com.mind.loginregisterapps;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.media.Image;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.util.Pair;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ImageButton btRegister;
+    private TextView tvLogin;
+
+    private EditText nick;
+    private EditText pass;
+/*
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btRegister  = findViewById(R.id.btRegister);
+        tvLogin     = findViewById(R.id.tvLogin);
+        btRegister.setOnClickListener(this);
+    }*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        nick = findViewById(R.id.nick);
+        pass= findViewById(R.id.pass);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                webconexion(nick.getText().toString(),pass.getText().toString());
+
+            }
+        });
+
+    }
+
+    private void webconexion(final String nick,final String pass) {
+
+        String WS_URL = "http://servicios.unnamed-chile.com/index.php/LOGIN_UBB";
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        StringRequest request = new StringRequest(
+                Request.Method.POST,
+                WS_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("LOG WS", response);
+                        try {
+                            JSONObject responseJson = new JSONObject(response);
+                            Snackbar.make(MainActivity.this.getCurrentFocus(), responseJson.getString("info"), Snackbar.LENGTH_SHORT)
+                                    .show();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("LOG WS", error.toString());
+
+            }
+        }
+        ) {
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("login",nick /*"test123"*/);
+                params.put("pass",pass/*"12345"*/);
+                return params;
+            }
+
+        };
+        requestQueue.add(request);
+    }
+
+
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void onClick(View v) {
+        if (v==btRegister){
+            Intent intent   = new Intent(MainActivity.this,RegisterActivity.class);
+            Pair[] pairs    = new Pair[1];
+            pairs[0] = new Pair<View,String>(tvLogin,"tvLogin");
+            ActivityOptions activityOptions = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,pairs);
+            startActivity(intent,activityOptions.toBundle());
+        }
+    }
+
+}
